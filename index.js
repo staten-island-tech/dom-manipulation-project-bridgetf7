fa2891f45a609e2544cdd77d0c747633ba81f60c
bridgetf7 <fbridget0404@gmail.com>
2022-11-10 13:13:56 -0500

    commit

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
+//object defining DOMSelectors
 const DOMSelectors = {
   birthYear: document.getElementById("birthYear"),
   submit: document.getElementById("submit"),
   reset: document.getElementById("reset"),
+  box: document.getElementById("box"),
+  clear: document.getElementById("clear")
 };
 
-console.log(DOMSelectors);
+birthYear.addEventListener("click", function (){
+  if (birthYear.textConten == 2031, 2019, 2007, 1995, 1983) {
+    DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `<p id="zodiactext"> You were born in the year of the pig!</p>
+    <button type="button" id="clear">Clear</button>`
+    );
+}
+else if (birthYear.textContent == 2020, 2008, 1996, 1984, 1972) {
+    DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `<p id="zodiactext"> You were born in the year of the rat!</p>
+    <button type="button" id="clear">Clear</button>`
+    );
+}
+});
 
-function submitform() {
-  const birthYear = document.getElementById("birthYear");
-  const zodiacResult  = 
-  "<p>You were born in the year of the pig!</p>";
-  birthYear.insertAdjacentHTML("afterend", zodiacResult);
+birthYear.addEventListener("click", function (){
+  box
 }
 
- function resetform() {
-      document.getElementById("form").reset();
-}
+//https://stackoverflow.com/questions/60476845/addeventlistener-and-if-statement-on-button-clicked
+/*
+DOMSelectors.submit.addEventListener("click", function (birthYear) {
 
+  if (birthYear.textContent == 2031, 2019, 2007, 1995, 1983) {
+      DOMSelectors.box.insertAdjacentHTML(
+          "beforeend",
+          `<p id="zodiactext"> You were born in the year of the pig!</p>
+      <button type="button" id="clear">Clear</button>`
+      );
+  }
+  else if (birthYear.textContent == 2020, 2008, 1996, 1984, 1972) {
+      DOMSelectors.box.insertAdjacentHTML(
+          "beforeend",
+          `<p id="zodiactext"> You were born in the year of the rat!</p>
+      <button type="button" id="clear">Clear</button>`
+      );
+  }
+  DOMSelectors.birthYear.value = "";
+})
+
+DOMSelectors.clear.value = "";
+*/
