d01d5ade6bac640cf255759db5da3ffefa0f1045
bridgetf7 <fbridget0404@gmail.com>
2022-11-15 13:11:47 -0500

    commit

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-//object defining DOMSelectors
 const DOMSelectors = {
-  birthYear: document.getElementById("birthYear"),
-  submit: document.getElementById("submit"),
-  reset: document.getElementById("reset"),
-  box: document.getElementById("box"),
-  clear: document.getElementById("clear")
-};
-
-
-DOMSelectors.submit.addEventListener("click", function (birthYear) {
-
-  if (birthYear == 2031, 2019, 2007, 1995, 1983) {
-      DOMSelectors.box.insertAdjacentHTML(
-          "beforeend",
-          `<p id="zodiactext"> You were born in the year of the pig!</p>
-      <button type="button" id="clear">Clear</button>`
-      );
-  }
-  else if (birthYear == 2020, 2008, 1996, 1984, 1972) {
-      DOMSelectors.box.insertAdjacentHTML(
-          "beforeend",
-          `<p id="zodiactext"> You were born in the year of the rat!</p>
-      <button type="button" id="clear">Clear</button>`
-      );
-  }
-  DOMSelectors.birthYear.value = "";
-})
-
-//https://stackoverflow.com/questions/60476845/addeventlistener-and-if-statement-on-button-clicked
+  
+}
