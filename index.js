7a99a21c2c6349b98cb02a673745094eae91e380
bridgetf7 <fbridget0404@gmail.com>
2022-11-17 21:33:57 -0500

    commit

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-//object defining DOMSelectors
 const DOMSelectors = {
-    birthYear: document.getElementById("birthYear"),
-    submit: document.getElementById("submit"),
-    reset: document.getElementById("reset"),
     box: document.getElementById("box"),
-    clear: document.getElementById("clear")
+    inputs: document.getElementById("inputs"),
+    albumName: document.getElementById("albumName"),
+    artistName: document.getElementById("artistName"),
+    buttons: document.getElementById("buttons"),
+    submit: document.getElementById("submit"),
+    image: document.getElementById("imageURL"),
+    clear: document.getElementById("clear"),
+    outputCard: document.getElementById("outputCard")
 };
+console.log(DOMSelectors)
+
+//does something when the "submit" button is hit
+DOMSelectors.submit.addEventListener("click", function () {
+    //provides the values for inputs 
+    const albumName = DOMSelectors.albumName.value;
+    const artistName = DOMSelectors.artistName.value;
+    const image = DOMSelectors.image.value;
+    const inputs = DOMSelectors.inputs.values;
+    //outputs a card when submit button is hit 
+    DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `<div class = "outputCard"> 
+       <p>${albumName}</p> 
+       <p><img src= ${image}></p> 
+       <p>${artistName}</p> 
+       <button type="button" id="clear">clear</button>
+       </div>`
+    )
+});
 
 
-DOMSelectors.submit.addEventListener("click", function (birthYear) {
 
-    if (birthYear == 2031, 2019, 2007, 1995, 1983) {
-        DOMSelectors.box.insertAdjacentHTML(
-            "beforeend",
-            `<p id="zodiactext"> You were born in the year of the pig!</p>
-        <button type="button" id="clear">Clear</button>`
-        );
-    }
-    else if (birthYear == 2020, 2008, 1996, 1984, 1972) {
-        DOMSelectors.box.insertAdjacentHTML(
-            "beforeend",
-            `<p id="zodiactext"> You were born in the year of the rat!</p>
-        <button type="button" id="clear">Clear</button>`
-        );
-    }
-    DOMSelectors.birthYear.value = "";
-})
+
+
+
+
+
