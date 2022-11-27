5e13187dec0f651e75490073baeefb90a2b66d64
bridgetf7 <fbridget0404@gmail.com>
2022-11-27 18:33:06 -0500

    commit

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
 const DOMSelectors = {
-    box: document.getElementById("box"),
-    inputs: document.getElementById("inputs"),
-    albumName: document.getElementById("albumName"),
-    artistName: document.getElementById("artistName"),
-    buttons: document.getElementById("buttons"),
     submit: document.getElementById("submit"),
-    image: document.getElementById("imageURL")
+    box: document.getElementById("container"),
+    albumName: document.querySelector("#albumName"),
+    artistName: document.querySelector("#artistName"),
+    image: document.querySelector("#image"),
+    form: document.getElementById("form"),
 };
-console.log(DOMSelectors)
 
-//does something when the "submit" button is hit
+console.log(DOMSelectors);
+
 DOMSelectors.submit.addEventListener("click", function (e) {
     e.preventDefault();
+    let albumName = DOMSelectors.albumName.value;
+    let artistName = DOMSelectors.artistName.value;
+    let image = DOMSelectors.image.value;
 
-    //provides the values for inputs 
-    const albumName = DOMSelectors.albumName.value;
-    const artistName = DOMSelectors.artistName.value;
-    const image = DOMSelectors.image.value;
-
-
-    //outputs a card when submit button is hit 
     DOMSelectors.box.insertAdjacentHTML(
-        "beforeend",
+        "afterend",
         `
-       <div id = "outputParent">
-       <div id = "outputCard"> 
-       <p>${albumName}</p> 
-       <p class=image ><img src= ${image}><br><br> 
-       <p>${artistName}</p> 
-       <button type="button" id="clear">clear</button>
-       </div>
-       </div>`
+    <section id="result">
+    <div class="outputCard">
+    <p>${albumName}</p>
+    <p><img src=${image}></p>
+    <p>${artistName}</p>
+    <button class="buttons" id="clear">clear</button>
+    </div>
+   </section>
+   `
     );
 
-    //clears input values on form
     DOMSelectors.albumName.value = "";
     DOMSelectors.artistName.value = "";
     DOMSelectors.image.value = "";
 
-    //clears result 
+    const result = document.getElementById("result");
     const clear = document.getElementById("clear");
-    const outputCard = document.getElementById("outputCard");
     clear.addEventListener("click", function () {
-        outputCard.remove();
+        result.remove();
     });
+
 });
